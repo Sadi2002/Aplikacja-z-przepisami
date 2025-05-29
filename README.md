e98bace5633d6ce13bc23f702e6d8cbf6149f1d8
Łukasz Sadowski <lukaszsadowski@MacBook-Pro-ukasz.local>
2025-05-29 19:36:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Przepisy na studia
+
+Recipe application with custom recipe uploads.
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e39c3c43-7601-4bdc-ad51-c5a3fe11a5ca';
+export const MODAL_CLOSE_SEC = 2.5;
